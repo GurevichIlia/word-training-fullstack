28ea3ac9813d6166ebdab59b1e665094643e5e8d
solfire <solfire@yamdex.ru>
2020-04-15 14:07:01 +0300

    try deploy again

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,11 +1,15 @@
 
-if (process.env.NODE_ENV === 'production') {
-      module.exports = require('./keys.prod')
+// if (process.env.NODE_ENV === 'production') {
+//       module.exports = require('./keys.prod')
 
-} else {
-      module.exports = require('./keys.dev')
+// } else {
+//       module.exports = require('./keys.dev')
+// }
+
+
+module.exports = {
+      mongoURI: 'mongodb+srv://solfire:4762071@cluster0-taobv.mongodb.net/test?retryWrites=true&w=majority',
+      jwt: 'jwt-token'
 }
-
-
 
 
