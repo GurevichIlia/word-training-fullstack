3f1419d19dcaed96697095376981b93de5d4f669
solfire <solfire@yamdex.ru>
2020-12-29 09:31:35 +0200

    FIxed: translation bugs

--- server/src/config/keys.dev.ts
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-      mongoURI: 'mongodb+srv://solfire:4762071@cluster0-taobv.mongodb.net/test?retryWrites=true&w=majority',
-      jwt: 'jwt-token',
-      GOOGLE_CLIENT_ID: '216967525964-ea6bh3tqp8vc6j4k5ifiol6a27u3s7kj.apps.googleusercontent.com',
-      GOOGLE_CLIENT_SECRET: 'JtX_XqDG4gioBiEE0oZkCEgv'
-}
-
-
--- server/src/config/keys.prod.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-      mongoURI: process.env.MONGO_URI || 'mongodb+srv://solfire:4762071@cluster0-taobv.mongodb.net/test?retryWrites=true&w=majority',
-      jwt: process.env.JWT || 'jwt-token',
-      GOOGLE_CLIENT_ID: '216967525964-ea6bh3tqp8vc6j4k5ifiol6a27u3s7kj.apps.googleusercontent.com',
-      GOOGLE_CLIENT_SECRET: 'JtX_XqDG4gioBiEE0oZkCEgv'
-}
-
